505baf096f1ab96241376085083cb16419ffa757
caohefei <hefei.cao@xiaoyangedu.com>
2021-10-29 20:02:46 +0800

    feat: init

---
+++ utils/renderTemplate.js
@@ -0,0 +1,38 @@
+import fs from 'fs'
+import path from 'path'
+
+import deepMerge from './deepMerge.js'
+import sortDependencies from './sortDependencies.js'
+
+function renderTemplate(src, dest) {
+  const stats = fs.statSync(src)
+
+  if (stats.isDirectory()) {
+    // if it's a directory, render its subdirectories and files recusively
+    fs.mkdirSync(dest, { recursive: true })
+    for (const file of fs.readdirSync(src)) {
+      renderTemplate(path.resolve(src, file), path.resolve(dest, file))
+    }
+    return
+  }
+
+  const filename = path.basename(src)
+
+  if (filename === 'package.json' && fs.existsSync(dest)) {
+    // merge instead of overwriting
+    const existing = JSON.parse(fs.readFileSync(dest))
+    const newPackage = JSON.parse(fs.readFileSync(src))
+    const pkg = sortDependencies(deepMerge(existing, newPackage))
+    fs.writeFileSync(dest, JSON.stringify(pkg, null, 2) + '\n')
+    return
+  }
+
+  if (filename.startsWith('_')) {
+    // rename `_file` to `.file`
+    dest = path.resolve(path.dirname(dest), filename.replace(/^_/, '.'))
+  }
+
+  fs.copyFileSync(src, dest)
+}
+
+export default renderTemplate
